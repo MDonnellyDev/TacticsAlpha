28d6d341f0b4a1b8bceabd97486cfceb619d77cf
Michael Cloonan <zxqamf@gmail.com>
2011-10-20 08:00:01 -0400

    Rendering HP/MP bars when hovering over an actor
    There has to be a better way/place to do all this font fidgeting and rectangle creation.

--- TacticsAlpha/src/com/tacalpha/actor/Actor.java
+++ TacticsAlpha/src/com/tacalpha/actor/Actor.java
@@ -3,10 +3,37 @@ package com.tacalpha.actor;
 import com.tacalpha.grid.GridPoint;
 
 public class Actor {
+	// Battle
 	private int xLocation;
 	private int yLocation;
+	private int currentHealth;
+	private int currentMana;
 
+	// Static
+	private int maxHealth;
+	private int maxMana;
+
+	// Configuration Methods
 	public Actor(int x, int y) {
+		this.xLocation = x;
+		this.yLocation = y;
+	}
+
+	public void configure(int maxHealth, int maxMana) {
+		this.maxHealth = maxHealth;
+		this.maxMana = maxMana;
+	}
+
+	public int getMaxHealth() {
+		return this.maxHealth;
+	}
+
+	public int getMaxMana() {
+		return this.maxMana;
+	}
+
+	// Battle Methods
+	public void updateLocation(int x, int y) {
 		this.xLocation = x;
 		this.yLocation = y;
 	}
@@ -15,14 +42,30 @@ public class Actor {
 		return new GridPoint(this.xLocation, this.yLocation);
 	}
 
-	public void updateLocation(int x, int y) {
-		this.xLocation = x;
-		this.yLocation = y;
+	public void prep() {
+		this.currentHealth = this.maxHealth;
+		this.currentMana = this.maxMana;
 	}
 
-	// public boolean move(Grid grid, Direction direction) {
-	// GridPoint destination = Direction.move(this.xLocation, this.yLocation,
-	// direction);
-	// return grid.moveActorIfPossible(this, destination);
-	// }
+	public void damage(int amount) {
+		this.currentHealth -= amount;
+		if (this.currentHealth < 0) {
+			this.currentHealth = 0;
+		}
+	}
+
+	public void heal(int amount) {
+		this.currentHealth += amount;
+		if (this.currentHealth > this.maxHealth) {
+			this.currentHealth = this.maxHealth;
+		}
+	}
+
+	public int getCurrentHealth() {
+		return this.currentHealth;
+	}
+
+	public int getCurrentMana() {
+		return this.currentMana;
+	}
 }
