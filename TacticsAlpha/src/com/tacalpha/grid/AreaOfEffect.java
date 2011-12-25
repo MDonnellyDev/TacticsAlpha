65218552f80a06ee34ae05ef6e0d8c75cc0a2f0e
Michael Cloonan <zxqamf@gmail.com>
2011-12-25 12:48:54 -0500

    Making AoE code more robust
    And added a new (tested and working) LineAOE. Moved a lot of code from the abstract class to the specific class. Each class should take care of its own code, because in the case of line attacks, the relative positions of affected squares could vary based on the position of the reference square.

--- TacticsAlpha/src/com/tacalpha/grid/AreaOfEffect.java
+++ TacticsAlpha/src/com/tacalpha/grid/AreaOfEffect.java
@@ -1,35 +1,15 @@
 package com.tacalpha.grid;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public abstract class AreaOfEffect {
-	protected Set<GridPoint> relativeLocations = new HashSet<GridPoint>();
-	private Grid grid;
+	protected Grid grid;
 
-	public final boolean contains(GridPoint referencePoint, GridPoint targetPoint) {
-		if (this.relativeLocations.isEmpty()) {
-			this.populateLocations();
-		}
-		return this.relativeLocations.contains(new GridPoint(targetPoint.getColumn() - referencePoint.getColumn(), targetPoint.getRow()
-				- referencePoint.getRow()));
-	}
+	public abstract boolean contains(GridPoint referencePoint, GridPoint targetPoint);
 
-	public final Set<GridPoint> getAllAffectedLocations(GridPoint referencePoint) {
-		Set<GridPoint> results = new HashSet<GridPoint>();
-		for (GridPoint relativeLocation : this.relativeLocations) {
-			int x = referencePoint.getColumn() + relativeLocation.getColumn();
-			int y = referencePoint.getRow() + relativeLocation.getRow();
-			if (this.grid.isInBounds(x, y)) {
-				results.add(new GridPoint(x, y));
-			}
-		}
-		return results;
-	}
+	public abstract Set<GridPoint> getAllAffectedLocations(GridPoint referencePoint);
 
 	void setGrid(Grid grid) {
 		this.grid = grid;
 	}
-
-	protected abstract void populateLocations();
 }
